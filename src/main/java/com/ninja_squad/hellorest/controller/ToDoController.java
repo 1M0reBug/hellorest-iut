9057f163d07819ba1c527fc193896dd57177e191
jordan <jordan.quagliatini@gmail.com>
2015-03-26 17:17:00 +0100

    Added Delete and Edit function on ToDoTasks with @BomberCraft

--- src/main/java/com/ninja_squad/hellorest/controller/ToDoController.java
+++ src/main/java/com/ninja_squad/hellorest/controller/ToDoController.java
@@ -1,11 +1,11 @@
 package com.ninja_squad.hellorest.controller;
 
 import com.ninja_squad.hellorest.model.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
@@ -20,13 +20,60 @@ public class ToDoController {
         tasks.add(task);
         return task;
     }
-    
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ToDoTask> edit(@PathVariable("id") long id, @RequestBody ToDoTask task) {
+
+        ToDoTask foundTask = this.getTaskById(id).getBody();
+        if (task != null) {
+            foundTask.label = task.label;
+            foundTask.priority = task.priority;
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(task, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ToDoTask> remove(@PathVariable("id") long id) {
+
+        ToDoTask task = this.getTaskById(id).getBody();
+        if (task != null) {
+            tasks.remove(task);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(task, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ToDoTask> getTaskById(@PathVariable("id") long id) {
+
+        for (ToDoTask task : tasks) {
+            if (task.id == id) {
+                return new ResponseEntity(task, HttpStatus.OK);
+            }
+        }
+
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public List<ToDoTask> list() {
         return tasks;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/priorities")
+    @ResponseStatus(HttpStatus.OK)
+    public Priority[] priorities() {
+        return Priority.values();
+    }
 
     // TODO Add a GET method listing existing tasks
 }
--- src/main/java/com/ninja_squad/hellorest/model/ToDoTask.java
+++ src/main/java/com/ninja_squad/hellorest/model/ToDoTask.java
@@ -1,17 +1,20 @@
 package com.ninja_squad.hellorest.model;
 
 public class ToDoTask {
-    
+
     public String label;
-    public String priority;
-    
-    public ToDoTask(){
-        this.priority = "DEFAULT";
+    public Priority priority;
+    public static long myId = 0;
+    public long id;
+
+    public ToDoTask() {
+        this.id = ++myId;
+        this.priority = Priority.DEFAULT;
         this.label = "Default";
     }
 
     public ToDoTask(String label) {
-        this.priority = "DEFAULT";
+        this();
         this.label = label;
     }
 }
